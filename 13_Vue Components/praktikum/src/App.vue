4d43cda3cef39e7b32bdc3248ecc0226276c8e9a
raihanerli <raihanerlianti@gmail.com>
2022-03-24 21:57:19 +0700

    add new task 13

---
+++ 13_Vue Components/praktikum/src/App.vue
@@ -0,0 +1,87 @@
+/* eslint-disable */
+<template>
+  <div id="App">
+    <h3>Todo List</h3>
+    <ol>
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.text }}
+        <input v-if="isEdit" type="text" v-model="newEdit" />
+        <button @click="deleteTodo(todo.id)">Hapus</button>
+        <button @click="editTodo(todo.id)">Edit</button>
+      </li>
+    </ol>
+    <input type="text" v-model="newTodo" />
+    <button @click="addTodo">Tambahkan</button>
+    <div v-if="this.todos.length >= 4">Hebat!</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      id: 1,
+      newTodo: "",
+      todos: [],
+      isEdit: false,
+      newEdit: "",
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id: this.id,
+        text: this.newTodo,
+      });
+      this.newTodo = "";
+      this.id++;
+    },
+    deleteTodo(hapusId) {
+      for (let index = 0; index < this.todos.length; index++) {
+        const element = this.todos[index];
+        // console.log(element.id, hapusId);
+        if (hapusId == element.id) {
+          //proses hapus dilakukan
+          // console.log('proses hapus dilakukan');
+          this.todos.splice(index, 1);
+          // console.log('proses hapus selesai');
+        }
+      }
+    },
+    editTodo(editId) {
+      // console.log(editId);
+      this.isEdit = true;
+      for (let index = 0; index < this.todos.length; index++) {
+        const element = this.todos[index];
+        console.log(element.id, editId);
+        if (editId == element.id) {
+          console.log(element);
+          console.log(this.newEdit); //sampe sini doloooooo
+          
+        }
+      }
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 30px;
+}
+
+input {
+  width: 80%;
+  margin-right: 80px;
+}
+
+ol {
+  margin: 10px;
+}
+</style>
---
+++ 13_Vue Components/praktikum/src/components/InputArray.vue
@@ -0,0 +1,57 @@
+/* eslint-disable */
+<template>
+  <div class="input">
+    <ol>
+      <li v-for="(todo, index) in todos" :key="todo.class">
+        {{ todo.Text }}
+        <button @click="deleteList(index)">Hapus</button>
+        <button @click="editList(index)">Edit</button>
+      </li>
+    </ol>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InputArray",
+  props: {
+    todos: String,
+  },
+  data() {
+    return {
+      todoBaru: ''
+    };
+  },
+  methods: {
+    addTodo() {
+      this.$emit("update-todo");
+    },
+    deleteList(index) {
+      this.todo.splice(index, 1);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.input {
+  width: 100vw;
+  /* display: flex !important; */
+}
+h3 {
+  margin: 40px 0 0;
+}
+ol {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
